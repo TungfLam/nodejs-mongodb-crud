bf266282ef3c860067f7044e2e1e81dbb8ef3fe5
Tung Lam <lamptph20282@fpt.edu.vn>
2024-08-01 15:17:39 +0700

    update

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const apiUserRoute = require("../modules/user/user.route");
-const apiTasksRoute = require("../modules/task/task.route");
-const apiResultRoute = require("../modules/result/result.route");
-const apiCreateDocument = require("../modules/tool/createDocument.route");
+const apiUserRoute = require('../modules/user/user.route');
+const apiTasksRoute = require('../modules/task/task.route');
+const apiResultRoute = require('../modules/result/result.route');
+const apiCreateDocument = require('../modules/tool/createDocument.route');
 
 router.use('/u', apiUserRoute);
 
 router.use('/task', apiTasksRoute);
 router.use('/result', apiResultRoute);
 
-router.use("/run", apiCreateDocument);
+router.use('/run', apiCreateDocument);
 
 module.exports = router;
